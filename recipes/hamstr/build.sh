d644cb14801464fd48a9159a788ed148cf17b4ef
trvinh <trvinh@gmail.com>
2019-12-17 15:32:59 +0100

    modified hamstr recipe

--- recipes/hamstr/build.sh
+++ recipes/hamstr/build.sh
@@ -6,11 +6,11 @@ TESTPKG_HOME=$PREFIX/$PKG_NAME-$PKG_VERSION
 
 # Copy source to the conda environment
 mkdir -p $TESTPKG_HOME
-cp -R $SRC_DIR/* $TESTPKG_HOME/
+cp -R $SRC_DIR/recipes/hamstr/bin/* $TESTPKG_HOME/
 
-# Create symbolic links for a5_miseq's launch script
+# Create symbolic links for setup_hamstr.sh script
 mkdir -p $BINARY_HOME
-ln -s $TESTPKG_HOME/bin/setup_hamstr.sh $BINARY_HOME/setup_hamstr
+ln -s $TESTPKG_HOME/setup_hamstr.sh $BINARY_HOME/setup_hamstr
 chmod +x $BINARY_HOME/setup_hamstr
 
 export PKGTEST="$BINARY_HOME"
